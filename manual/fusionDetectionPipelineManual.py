55d1948a1b424308fd0afbb922a0069742252f9b
Ubuntu <lincoln.harris@czbiohub.org>
2019-01-09 22:14:18 +0000

    added runTrinity routine -- working now!

--- manual/fusionDetectionPipelineManual.py
+++ manual/fusionDetectionPipelineManual.py
@@ -17,7 +17,8 @@ pd.options.mode.chained_assignment = None
 
 #////////////////////////////////////////////////////////////////////
 # get_fastq1()
-#	what does this fucker do? 
+#	return the full path of a R1 fastq file, given an s3 bucket, prefix
+#	and cell name
 #////////////////////////////////////////////////////////////////////
 
 def get_fastq1(cell):
@@ -34,10 +35,11 @@ def get_fastq1(cell):
 
 #////////////////////////////////////////////////////////////////////
 # get_fastq2()
-#	what does this fucker do? 
+#	return the full path of a R2 fastq file, given an s3 bucket, prefix
+#	and cell name
 #////////////////////////////////////////////////////////////////////
 
-def get_fastq1(cell):
+def get_fastq2(cell):
     s3_location = prefix + cell 
     lines = get_ipython().getoutput('aws s3 ls $s3_location') 
     
@@ -50,11 +52,12 @@ def get_fastq1(cell):
     return s3_location + fastq_basename
 
 #////////////////////////////////////////////////////////////////////
-# driver()
-#	what does this fucker do? 
+# getCellTable()
+#	set up a table with cell name and full s3 paths to both fastqs, for
+#	every cell in a given run 
 #////////////////////////////////////////////////////////////////////
 
-def driver():
+def getCellTable(prefix):
 	txt = 'runX_cells.txt'
 	get_ipython().getoutput('aws s3 ls $prefix > $txt')
     
@@ -63,21 +66,37 @@ def driver():
 
 	cells_df['input_fastq1'] = cells_df['cell_name'].map(get_fastq1)
 	cells_df['input_fastq2'] = cells_df['cell_name'].map(get_fastq2)
-	cells_df['sample_id'] = cells_df.cell_name.str.strip('/') # get rid of forward slashes and add 'sample_id' col
+	cells_df['sample_id'] = cells_df.cell_name.str.strip('/') # get rid of forward slashes
+
+	return(cells_df)
+
+#////////////////////////////////////////////////////////////////////
+# runTrinity()
+#	actually runs STAR-Fus, from within the Trinity docker container, 
+#	given input_fastq1, input_fastq2 and cell_name
+#////////////////////////////////////////////////////////////////////
+
+def runTrinity(row):
+	fq1 = row['input_fastq1']
+	fq2 = row['input_fastq2']
+	cell = row['sample_id']
 
 	# get current fastqs
-	get_ipython().system('aws s3 cp s3://darmanis-group/singlecell_lungadeno/non_immune/nonImmune_fastqs_9.27/170125/A10_1001000293/A10_1001000293_R1_001.fastq.gz .')
-	get_ipython().system('aws s3 cp s3://darmanis-group/singlecell_lungadeno/non_immune/nonImmune_fastqs_9.27/170125/A10_1001000293/A10_1001000293_R2_001.fastq.gz .')
+	get_ipython().system('aws s3 cp $fq1 .')
+	get_ipython().system('aws s3 cp $fq2 .')
 
 	# run STAR-fus, from docker container
-	get_ipython().system('sudo docker run -v `pwd`:/data --rm trinityctat/ctatfusion /usr/local/src/STAR-Fusion/STAR-Fusion --left_fq /data/*_R1_001.fastq.gz --right_fq /data/*_R2_001.fastq.gz --genome_lib_dir /data/ctat_genome_lib_build_dir -O /data/StarFusionOut --FusionInspector validate --examine_coding_effect --denovo_reconstruct')
+	get_ipython().system('sudo docker run -v `pwd`:/data --rm trinityctat/ctatfusion /usr/local/src/STAR-Fusion/STAR-Fusion --left_fq /data/*_R1_001.fastq.gz --right_fq /data/*_R2_001.fastq.gz --genome_lib_dir /data/ctat_genome_lib_build_dir -O /data/StarFusionOut/$cell --FusionInspector validate --examine_coding_effect --denovo_reconstruct')
 
 	# remove current fastqs
 	get_ipython().system('rm *.fastq.gz')
 
 #////////////////////////////////////////////////////////////////////
 # main()
-#	what does this fucker do? 
+#	pop life
+# 	everybody needs a thrill!
+# 	pop life
+#	we all got space 2 fill!
 #////////////////////////////////////////////////////////////////////
 
 # get list of all the runs
@@ -91,11 +110,12 @@ runs_df = pd.read_table(f, delim_whitespace=True, header=None, names=['is_prefix
 # add a full_path col
 runs_df['full_path'] = 's3://darmanis-group/singlecell_lungadeno/non_immune/nonImmune_fastqs_9.27/' + runs_df['run_name']
 
-for i in range(0, len(runs_df.index)-1): # -2 bc i have two vcf out folders
+for i in range(0, len(runs_df.index)-1): # -1 here? 
 	global prefix # dont like this
 	prefix = runs_df['full_path'][i]
 	print(prefix)
-	driver(prefix)
+	currCells = getCellTable(prefix)
+	currCells.apply(runTrinity, axis=1) # send the currCell df to runTrinity func
 
 #////////////////////////////////////////////////////////////////////
 #////////////////////////////////////////////////////////////////////
