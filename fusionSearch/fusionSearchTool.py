59084e3bc0e90ecc790bd59c729f50702d25acfc
Lincoln Harris <lincoln.harris@czbiohub.org>
2019-02-20 11:06:37 -0800

    working!!

--- fusionSearch/fusionSearchTool.py
+++ fusionSearch/fusionSearchTool.py
@@ -16,6 +16,7 @@ import os
 #////////////////////////////////////////////////////////////////////
 def searchFunc(row, FOI):
 	cellFile = row['name']
+	cellName = cellFile.split('_')[0] + '_' + cellFile.split('_')[1]
 
 	cwd = os.getcwd()
 	path = cwd + '/' + 'fusion_prediction_files/' + cellFile
@@ -25,9 +26,9 @@ def searchFunc(row, FOI):
 	fusionsList = list(curr_fusions['#FusionName'])
 
 	if str(FOI) in fusionsList:
-		outputRow = pd.DataFrame([[cellFile, 1]], columns=['cellName', 'fusionPresent_bool'])
+		outputRow = pd.DataFrame([[cellName, 1]])
 	else:
-		outputRow = pd.DataFrame([[cellFile, 0]], columns=['cellName', 'fusionPresent_bool'])
+		outputRow = pd.DataFrame([[cellName, 0]])
 
 	return outputRow
 
@@ -36,18 +37,21 @@ def searchFunc(row, FOI):
 #
 #
 #////////////////////////////////////////////////////////////////////
+global colNames
 
 pos_test = 'CD24P4--QPRT'
 queryStr = 'ALK--EML4'
 queryStr_rev = 'EML4--ALK'
 
+colNames = ['cellName', 'fusionPresent_bool']
+
 cellFiles = os.listdir('./fusion_prediction_files')
 cellFiles_df = pd.DataFrame(data=cellFiles, columns=['name']) # need to convert to df before apply call
 
-outputDF_init = pd.DataFrame(columns=['cellName', 'fusionPresent_bool']) 	
-
 outputRows = cellFiles_df.apply(searchFunc, axis=1, args=(pos_test,))
-outputDF = outputDF_init.append(outputRows, ignore_index=True)
+outputRows_list = list(outputRows) # for some reason need to convert to a list before concatting
+
+outputDF = pd.concat(outputRows_list, ignore_index=True)
 outputDF.to_csv('testOut.csv', index=False)
 
 #////////////////////////////////////////////////////////////////////
